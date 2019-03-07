8e08faa65a8566bea22de14d71676b532cade233
Rodrigo <rodrigooliveiravarela@gmail.com>
2019-03-06 23:47:35 -0300

    Adicionado botao antes da section

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ class App extends Component {
       <div className="container-fluid">
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
 
-          <a className="navbar-brand" href="#"><i class="fas fa-eye"></i>&nbsp;Logo</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
+            aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-          <div className="collapse" id="navbarMainToggler">
+          <section className="collapse navbar-collapse" id="navbarMainToggler">
+            <a className="navbar-brand" href="#"><i className="fas fa-eye"></i>&nbsp;Logo</a>
             <div className="navbar-nav">
               <a className="nav-item nav-link" href="#">Home</a>
               <a className="nav-item nav-link" href="#">Chanel</a>
               <a className="nav-item nav-link" href="#">Login</a>
             </div>
-          </div>
+          </section>
 
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
-            aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
 
         </nav>
       </div>
